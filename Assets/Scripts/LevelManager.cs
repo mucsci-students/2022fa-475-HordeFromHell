567f9e3c1182443e41e6044540f88b55a4fcc105
BrandonTBaldwin <89534456+BrandonTBaldwin@users.noreply.github.com>
2022-09-26 17:45:49 -0400

    Menu Update
    - Added a button for map selection, we only have 1 map right now so its more for display until we get another
    - Changed a UI element from toggle to drop down selection because it could have more than 1 option(Not functional yet until later in project)
    - Took out the FPSController because it wasn't needed for the main menu
    - Added the ability to change scenes so when this is merged with the map, we can actually be put into the game

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "Forest";
+
+	public void LoadGame()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+
+	public void SetMap(string scene)
+	{
+		sceneToLoad = scene;
+	}
+}
